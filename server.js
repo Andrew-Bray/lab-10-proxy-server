c3c6e7019a1ea8ed5e9905e062e73f8973a4faa5
Andrew-Bray <amichaelbray@gmail.com>
2020-11-09 17:50:35 -0800

    weather attempt

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./lib/client').connect();
 const app = require('./lib/app');
 const PORT = process.env.PORT || 7890;
 
@@ -6,6 +7,7 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
+
 
 
 
--- utils.js
+++ utils.js
@@ -7,6 +7,19 @@ function mungeLocation(location) {
   };
 }
 
+function mungeWeather(location) {
+
+  location.data.map(day => {
+    console.log(day);
+  });
+  //forecast: location[0].data.weather.description,
+
+  //time: location[0].datetime
+
+}
+
+
 module.exports = {
-  mungeLocation
+  mungeLocation,
+  mungeWeather
 };
